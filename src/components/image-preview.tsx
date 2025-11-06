683c6f4edabd877ad1f4fe4372e27df558598009
Bruno José <bru.matias100@gmail.com>
2025-11-06 13:36:54 -0300

    feat: creating photo widget component

---
+++ src/components/image-preview.tsx
@@ -0,0 +1,26 @@
+import type { ComponentProps } from "react";
+import { tv } from "tailwind-variants";
+
+export const imagePreviewVariants = tv({
+  base: 'rounded-lg overflow-hidden',
+});
+
+export const imagePreviewImageVariants = tv({
+  base: 'w-full h-full object-cover'
+});
+
+interface ImagePreviewProps extends ComponentProps<'img'> {
+  imageClassName?: string
+}
+
+export default function ImagePreview({
+  className,
+  imageClassName,
+  ...props
+}: ImagePreviewProps) {
+  return (
+    <div className={imagePreviewVariants({ className })}>
+      <img className={imagePreviewImageVariants({ className: imageClassName })} {...props} />
+    </div>
+  )
+}
--- src/pages/page-home.tsx
+++ src/pages/page-home.tsx
@@ -1,9 +1,29 @@
-import Text from "../components/text";
+import Container from "../components/container";
+import type { Photo } from "../contexts/models/photo";
+import PhotoWidget from "../contexts/photos/components/photo-widget";
 
 export default function PageHome() {
   return (
-    <>
-      <Text variant="heading-medium">Página Inicial</Text>
-    </>
+    <Container>
+      <div className="grid grid-cols-4 gap-9">
+        <PhotoWidget
+          photo={{
+            id: '123',
+            title: 'Teste',
+            imageId: 'portrait-tower.png',
+            albums: [
+              { id: '3421', title: 'Album 1' },
+              { id: '123', title: 'Album 2' },
+              { id: '456', title: 'Album 3' },
+            ]
+          }}
+        />
+
+        <PhotoWidget
+          photo={{} as Photo}
+          loading
+        />
+      </div>
+    </Container>
   )
 }
