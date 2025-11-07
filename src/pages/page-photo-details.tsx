31336c847f824ef8e67d8ce72802d205d727847b
Bruno José <bru.matias100@gmail.com>
2025-11-07 15:14:37 -0300

    feat: creating albums list selectable component

--- src/pages/page-photo-details.tsx
+++ src/pages/page-photo-details.tsx
@@ -1,7 +1,10 @@
 import { useParams } from "react-router";
+import Button from "../components/button";
 import Container from "../components/container";
+import ImagePreview from "../components/image-preview";
 import Skeleton from "../components/skeleton";
 import Text from "../components/text";
+import AlbumsListSelectable from "../contexts/albums/components/albums-list-selectable";
 import PhotosNavigator from "../contexts/albums/components/photo-navigation";
 import type { Photo } from "../contexts/photos/models/photo";
 
@@ -24,7 +27,7 @@ export default function PagePhotoDetails() {
     <Container>
       <header className="flex text-center justify-between gap-8 mb-8">
         {!isLoadingPhoto ? (
-          <Text variant="heading-large">
+          <Text as="h2" variant="heading-large">
             {photo?.title}
           </Text>
         ) : (
@@ -33,6 +36,48 @@ export default function PagePhotoDetails() {
 
         <PhotosNavigator />
       </header>
+
+      <div className="grid grid-cols-[21rem_1fr] gap-24">
+        <div className="space-y-3">
+          {!isLoadingPhoto ? (
+            <ImagePreview
+              src={`/images/${photo?.imageId}`}
+              title={photo?.title}
+              imageClassName="h-[21rem]"
+            />
+          ) : (
+            <Skeleton className="h-[21rem]" />
+          )}
+
+          {!isLoadingPhoto ? (
+            <Button variant="destructive">
+              Excluir
+            </Button>
+          ) : (
+            <Skeleton className="w-28 h-10" />
+          )}
+        </div>
+
+        <div className="py-3">
+          <Text
+            as="h3"
+            variant="heading-medium"
+            className="mb-6"
+          >
+            Álbuns
+          </Text>
+
+          <AlbumsListSelectable
+            photo={photo}
+            albums={[
+              { id: '3421', title: 'Album 1' },
+              { id: '123', title: 'Album 2' },
+              { id: '456', title: 'Album 3' },
+            ]}
+            loading={isLoadingPhoto}
+          />
+        </div>
+      </div>
     </Container>
   )
 }
